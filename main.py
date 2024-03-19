94c0541a3bd43bccf33107d294b5d97e20712e74
Yu chen <lycymx@gmail.com>
2024-03-18 23:30:09 -0700

    init

---
+++ main.py
@@ -0,0 +1,7 @@
+import ttkbootstrap as ttk
+from tkinter import filedialog
+from tkinter.messagebox import showerror, askyesno
+from tkinter import colorchooser
+from PIL import Image, ImageOps, ImageTk, ImageFilter, ImageGrab
+import pyqrcode
+import cv2
