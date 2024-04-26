c6af25ddcd4bb8eb25c4b23cb9e6b006b6689c7e
jahager <jahager@cpp.edu>
2024-04-26 14:46:28 -0700

    Removed Unused Imports

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import ttkbootstrap as ttk
 import tkinter
-from tkinter import filedialog
-from tkinter.messagebox import showerror, askyesno
-from tkinter import colorchooser
-from PIL import Image, ImageOps, ImageTk, ImageFilter, ImageGrab
-import pyqrcode
-import cv2
 
 # Process command line arguments
 import cli_image
